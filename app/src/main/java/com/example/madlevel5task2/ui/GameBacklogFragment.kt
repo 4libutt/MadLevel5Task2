5875c10e38a2e1b2c6750a53df6169f45cbd9327
4libutt <ali@vergelijkgroep.nl>
2020-11-23 18:41:14 +0100

    added ui package and placed files in correct package

--- app/src/main/java/com/example/madlevel5task2/ui/GameBacklogFragment.kt
+++ app/src/main/java/com/example/madlevel5task2/ui/GameBacklogFragment.kt
@@ -1,12 +1,12 @@
-package com.example.madlevel5task2
+package com.example.madlevel5task2.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.madlevel5task2.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 /**
@@ -26,7 +26,8 @@ class GameBacklogFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<FloatingActionButton>(R.id.fabAdd).setOnClickListener {
             findNavController().navigate(
-                R.id.action_GameBacklogFragment_to_AddGameFragment)
+                R.id.action_GameBacklogFragment_to_AddGameFragment
+            )
         }
     }
 }
